df330556c84406c49c37d317c54b2d9bbea3bb90
Jamie Mair <jamie.mair@hotmail.co.uk>
2021-10-11 14:06:23 +0100

    Added a starting point function
    Added a function to illustrate overall design of the function. This includes adding the ability to hook into any part of the process. A lot of the library will involve choosing default implementations.

--- src/TPS.jl
+++ src/TPS.jl
@@ -1,5 +1,50 @@
 module TPS
 
-# Write your package code here.
+function do_nothing() nothing end
+
+function get_init_fn(cache, config) end
+function get_stopping_fn(cache, config) end
+function get_step_fn(cache, config) end
+function get_pre_init_hook(cache, config) do_nothing end
+function get_post_init_hook(cache, config) do_nothing end
+function get_pre_step_hook(cache, config) do_nothing end
+function get_post_step_hook(cache, config) do_nothing end
+function get_final_hook(cache, config) do_nothing end
+
+function run!(cache, config)
+    # Extract all the functions you need based on the configuration and cache
+    init_fn! = get_init_fn(cache, config)
+    stop_fn! = get_stopping_fn(cache, config)
+    step_fn! = get_step_fn(cache, config)
+
+    # Load the hook functions
+    pre_init_hook! = get_pre_init_hook(cache, config)
+    post_init_hook! = get_post_init_hook(cache, config)
+
+    pre_step_hook! = get_pre_step_hook(cache, config)
+    post_step_hook! = get_post_step_hook(cache, config)
+
+    final_hook! = get_final_hook(cache, config)
+
+    # Compile the inner loop, hopefully removing unneeded hooks
+    function inner_loop!()
+        pre_step_hook!()
+        step_fn!()
+        post_step_hook!()
+    end
+
+    pre_init_hook!()
+    init_fn!()
+    post_init_hook!()
+    
+    while !stop_fn!()
+        inner_loop!()
+    end
+
+    final_hook!()
+
+    nothing
+end
+
 
 end
