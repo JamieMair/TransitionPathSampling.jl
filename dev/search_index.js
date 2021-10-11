b72760b6babd035dad656fe8d0f1a07a3fca0d1e
Documenter.jl <documenter@juliadocs.github.io>
2021-10-11 11:22:24 +0000

    build based on 477d418

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TPS","category":"page"},{"location":"#TPS","page":"Home","title":"TPS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TPS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TPS]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TPS","category":"page"},{"location":"#TPS","page":"Home","title":"TPS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TPS. This is a module written by Jamie Mair.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TPS]","category":"page"}]
 }
