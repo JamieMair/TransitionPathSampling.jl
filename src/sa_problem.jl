7c701d15745d42ade2814d64a49deb34fdc807cb
Jamie Mair <jamie.mair@hotmail.co.uk>
2021-11-02 10:43:47 +0000

    Added different types for the SimpleObservable

--- src/sa_problem.jl
+++ src/sa_problem.jl
@@ -14,27 +14,28 @@ function get_observable(problem::SAProblem)
     return problem.observable
 end
 
-struct SimpleObservable{T} <: AbstractObservable
-    observe::Function
+struct SimpleObservable{T<:Function} <: AbstractObservable
+    observe::T
 end
 
 function observe(observable::AbstractObservable, state) error("Not implemented") end
-function observe(observable::SimpleObservable{T}, state::T) where {T}
+function observe(observable::SimpleObservable, state::T) where {T}
     return observable.observe(state)
 end
 
 abstract type AbstractSolution <: TPSSolution end
 
-struct SimpleSolution{T}
+mutable struct SimpleSolution{T, S}
     observations::Vector{T}
     problem::TPSProblem
     algorithm::TPSAlgorithm
-    state
+    state::S
 end
 # Forward these methods so that the solution can be plotted
 @forward SimpleSolution.observations (size, getindex, setindex!, IndexStyle, iterate, similar, push!, pop!)
 
 abstract type AbstractMetropolisHastingsAlg <: TPSAlgorithm end
+
 function init_solution(alg::AbstractMetropolisHastingsAlg, problem::TPSProblem, args...; kwargs...)
     observable = get_observable(problem)
     initial_state = get_initial_state(problem)
@@ -42,7 +43,9 @@ function init_solution(alg::AbstractMetropolisHastingsAlg, problem::TPSProblem, 
 
     # By default, have a simple solution
     # TODO: Add some configuration options here
-    sol = SimpleSolution([initial_observable])
+    # sol = SimpleSolution([initial_observable])
 
     return sol
 end
+
+export SAProblem, SimpleObservable, observe, SimpleSolution, init_solution
