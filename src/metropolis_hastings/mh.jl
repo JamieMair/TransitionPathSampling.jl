b67efa0097ae6ea8f235de1c4204eaac4e8c413a
Jamie Mair <jamie.mair@hotmail.co.uk>
2021-11-16 13:10:36 +0000

    Exported the shooting constructor

--- src/metropolis_hastings/mh.jl
+++ src/metropolis_hastings/mh.jl
@@ -6,6 +6,6 @@ abstract type AbstractMetropolisHastingsAlg <: TPSAlgorithm end
 include("mh_sa_alg.jl")
 include("mh_trajectory_alg.jl")
 
-export MetropolisHastingsAlgorithm, get_guassian_mh_alg
+export MetropolisHastingsAlgorithm, get_guassian_mh_alg, get_shooting_mh_alg
 
 end
