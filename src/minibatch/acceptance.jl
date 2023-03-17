795ab05d5652aede96e7235f2b909d63728ca031
Jamie Mair <JamieMair@users.noreply.github.com>
2023-03-17 14:47:04 +0000

    Fixed histogram cache forwarding for minibatch acceptance

--- src/minibatch/acceptance.jl
+++ src/minibatch/acceptance.jl
@@ -118,7 +118,7 @@ end
 has_histogram(::AbstractMinibatchAcceptanceCache) = false
 has_histogram(::MinibatchAcceptanceCacheWithHistogram) = true
 get_histogram(c::MinibatchAcceptanceCacheWithHistogram) = c.histogram
-Lazy.@forward MinibatchAcceptanceCacheWithHistogram.cache get_all_indices, get_indices, get_total_samples, get_error_tol, get_batch_size, get_correction_dist, get_cutoff, get_offset
+Lazy.@forward MinibatchAcceptanceCacheWithHistogram.cache get_all_indices, get_indices, increment_indices!, get_total_samples, get_error_tol, get_batch_size, get_correction_dist, get_cutoff, get_offset
 
 function increment_indices!(cache::MinibatchAcceptanceCache)
     cache.offset_index = (cache.offset_index + cache.batch_size + 1) % cache.total_samples - 1
